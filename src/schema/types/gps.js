dce1c5a92d6d74c307dffc1c23b8df2b0e054b95
Anton Komarenko <mi3ta@sent.as>
2018-11-10 18:06:14 +0200

    :sparkles: Adds gps gql type

---
+++ src/schema/types/gps.js
@@ -0,0 +1,13 @@
+// GraphQL Language Cheat Sheet
+// https://raw.githubusercontent.com/sogko/graphql-shorthand-notation-cheat-sheet/master/graphql-shorthand-notation-cheat-sheet.png
+export default `
+  type GPS {
+    lat: String
+    lng: String
+  }
+
+  input GPSPayload {
+    lat: String
+    lng: String
+  }
+`
