108fa8b2083c81c913fb6a7c9d62441722e6c42f
Anton Komarenko <mi3ta@sent.as>
2018-11-10 18:07:08 +0200

    :sparkles: Adds some useful scalars

---
+++ src/schema/types/scalars.js
@@ -0,0 +1,6 @@
+// GraphQL Language Cheat Sheet
+// https://raw.githubusercontent.com/sogko/graphql-shorthand-notation-cheat-sheet/master/graphql-shorthand-notation-cheat-sheet.png
+export default `
+  scalar DateTime
+  scalar Email
+`
